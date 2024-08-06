c25dcfd582a7518d1c0200336c952060cf19eef4
matias delongaro <matiasadelongaro@gmail.com>
2024-08-06 12:05:01 -0300

    update

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const menuIcon = document.getElementById("menuIcono");
+    const menu = document.getElementById("navBar")
+
+
+menuIcon.addEventListener("click", function(){
+    menu.classList.toggle('active');
+});
+})
