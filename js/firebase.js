188deb828ca7ccf8cd351e0d29c8f89a2e914788
matias delongaro <matiasadelongaro@gmail.com>
2024-08-09 17:30:49 -0300

    subida

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBOuzKDhbhEkVbW58ZQrtR_NAFAIMF7Ljk",
+    authDomain: "portfolio-matias-4c870.firebaseapp.com",
+    projectId: "portfolio-matias-4c870",
+    storageBucket: "portfolio-matias-4c870.appspot.com",
+    messagingSenderId: "871687476043",
+    appId: "1:871687476043:web:d3ca82f7ae8290b43b0ece"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import '../js/firebase.js'
+
 document.addEventListener('DOMContentLoaded', function(){
     const menuIcon = document.getElementById("menuIcono");
     const menu = document.getElementById("navBar")
@@ -8,3 +10,14 @@ menuIcon.addEventListener("click", function(){
     
 });
 })
+const formulario = document.querySelector('#formulario')
+
+const procesaTodo = (event) =>{
+    event.preventDefault();
+    const datos = new FormData(event.target)
+
+    const datosCompletos = Object.fromEntries(datos.entries())
+    console.log(JSON.stringify(datosCompletos))
+}
+
+formulario.addEventListener('submit', procesaTodo);
